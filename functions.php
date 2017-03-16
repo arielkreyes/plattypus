36da8a6d32d22e7bbe72c72794ccba3201fc90d1
arielkreyes <ariel.k.reyes@hotmail.com>
2017-03-16 11:55:50 -0700

    Day....I forgot
    Balh

--- functions.php
+++ functions.php
@@ -7,4 +7,47 @@ add_theme_support('post-thumbnails');
 
 add_theme_support( 'post-formats', array( 'quote', 'link', 'audio', 'video',
 'image', 'gallery', 'aside', 'status' ) );
+
+add_theme_support( 'custom-background');
+//don't forget to show the header image in the header.php file
+add_theme_support( 'custom-header', array(
+                                        'width'      => 960,
+                                        'height'     => 700,
+                                        'flex-width' => true,
+                                        'flex-height'=> true,
+));
+
+//don't forget the custom_logo() to display it in your theme
+add_theme_support( 'custom-logo', array(
+  'width' => 200,
+  'height'  => 200,
+));
+
+//better RSS feed links. a must-have if use a blog
+add_theme_support( 'automatic-feed-links');
+
+//improve the markup of Wordpress generated code
+add_theme_support( 'html5', array('search-form', 'comment-list', 'comment-form', 'gallery', 'caption', ));
+
+//improve title tag for SEO(feeds google :D) and inserts using the wp_head();
+//remember to remove the <title> from header.php
+add_theme_support('title-tag');
 //no close ze php to avoid whitespace issues
+
+/**
+  * Make the excerpts better - customize the number of words and change [...]
+  * @see - link back to resource (if)used
+*/
+function platty_ex_length(){
+  if( is_search()){
+    return 20; //number of words wanted in excerpt
+  }else{
+  return 75; //number of words wanted in excerpt
+  }
+}
+add_filter( 'excerpt_length', 'platty_ex_length' );
+
+function platty_readmore(){
+  return ' <a href="' . get_permalink() . '" class="read-more" title="Keep Reading This Post">Read More...</a>';
+}
+add_filter( 'excerpt_more', 'platty_readmore' );
--- header.php
+++ header.php
@@ -3,14 +3,18 @@
   <head>
     <meta charset="<?php bloginfo( 'charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TITLE</title>
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <?php wp_head(); //this is a hook. required for plugins admin bar to work ?>
   </head>
 <body <?php body_class(); ?>>
   <!-- puts in a class for you to specify which page you are on. -->
-<header role="banner" id="header">
+<header role="banner" id="header" style="background-image:url(<?php header_image(); ?>)">
   <div class="header-bar">
+    <?php
+    if(function_exists('the_custom_logo')){
+      the_custom_logo();
+    }
+    ?>
     <h1 class="site-title"><a href="<?php echo home_url(); ?>">
       <?php bloginfo( 'name' ); ?>
     </a></h1>
